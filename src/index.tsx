cdac287442e8939f0b5c54acd2285ca74c360a19
jinzhongjia <mail@nvimer.org>
2026-07-05 19:48:08 +0800

    chore: 精简入口文件注释以降低维护噪音

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,7 @@ import { Boundary } from "./Boundary";
 import { Page, ROUTE } from "./Page";
 import { QAM } from "./QAM";
 
-// 入口:只做接线。QAM 面板见 QAM.tsx,大屏页见 Page.tsx。
 export default definePlugin(() => {
-  // 路由只在插件加载时注册一次(在事件处理里 addRoute 会重挂载 QAM 面板、复位 state)。
-  // 大屏入口按钮仍按 provider 选择显隐,故“未选 provider 不进大屏”的体验不变。
   routerHook.addRoute(ROUTE, () => (
     <Boundary>
       <Page />
